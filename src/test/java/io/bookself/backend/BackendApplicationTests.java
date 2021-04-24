559b599d8ac8c3646e8ac8e5291bdba6784a824d
Kyle Olson <kyle@k-olson.org>
2021-04-23 22:18:50 -0400

    initial bootstrapping of spring boot and CI job

---
+++ src/test/java/io/bookself/backend/BackendApplicationTests.java
@@ -0,0 +1,17 @@
+package io.bookself.backend;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	@DisplayName("Remove this when we start coding...")
+	void testTheTest() {
+		Assertions.assertDoesNotThrow(() -> BackendApplication.somethingToTest("b"));
+	}
+
+}
