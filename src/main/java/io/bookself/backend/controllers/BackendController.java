1ac4a849d3fa964170f03e07c9ece6d23f398c9b
Kyle Olson <kyle@k-olson.org>
2021-04-25 23:02:42 -0400

    tweaks

--- src/main/java/io/bookself/backend/controllers/BackendController.java
+++ src/main/java/io/bookself/backend/controllers/BackendController.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class BackendController {
 
+    /**
+     * DO NOT REMOVE - This is used as the healthcheck endpoint for the load balancer.
+     */
     @GetMapping("/ping")
     public String ping() {
         return "pong";
