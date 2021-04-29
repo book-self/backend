1056cd339f816a84ade03f3eb5172afd966a283b
nardost <nardos.tessema@gmail.com>
2021-04-29 13:07:56 -0500

    buildSrc included. Dependencies parameterized. Plugin management defined in settings.gradle

---
+++ buildSrc/src/main/java/config/Project.java
@@ -0,0 +1,7 @@
+package config;
+
+public class Project {
+    public static final String groupName = "io.bookself";
+    public static final String projectVersion = "0.0.1-SNAPSHOT";
+    public static final String javaVersion = "11";
+}
