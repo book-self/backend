7696912b6007f7cb6df0e854454870fd99ff95f7
Lisa Sun <c.lisa.sun@gmail.com>
2021-05-09 18:20:45 -0500

    Altered the mappings slighlty. They are changeable. User is able to be created and saved to database

--- apis/src/main/java/xyz/bookself/controllers/user/UserController.java
+++ apis/src/main/java/xyz/bookself/controllers/user/UserController.java
@@ -11,10 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 import xyz.bookself.books.domain.Book;
 import xyz.bookself.config.BookselfApiConfiguration;
+import xyz.bookself.users.domain.BookList;
+import xyz.bookself.users.domain.BookListEnum;
 import xyz.bookself.users.domain.User;
+import xyz.bookself.users.repository.BookListRepository;
 import xyz.bookself.users.repository.UserRepository;
 
 import java.time.LocalDate;
+import java.util.UUID;
 
 @RestController
 public class UserController {
@@ -22,28 +26,31 @@ public class UserController {
     private final BookselfApiConfiguration apiConfiguration;
     private final UserRepository userRepository;
 
+
     @Autowired
     public UserController(BookselfApiConfiguration configuration,UserRepository repository) {
         this.userRepository = repository;
         this.apiConfiguration = configuration;
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/user/{id}")
     public ResponseEntity<User> getUser(@PathVariable int id) {
         final User user = userRepository.findById(id).orElseThrow();
         return new ResponseEntity<>(user, HttpStatus.OK);
     }
 
-    @PostMapping("/newUser")
-    public ResponseEntity<User> createNewUser(){
+    @GetMapping("/newUser")
+    public String createNewUser(){
         User newUser = new User();
-        newUser.setId(123);
         newUser.setUsername("dummyUserName");
         newUser.setEmail(("dummy@dummy.com"));
+        newUser.setPasswordHash(UUID.randomUUID().toString().replace("-", ""));
+        
         LocalDate date = LocalDate.now();
         newUser.setCreated(date);
-        //userRepository.save(newUser);
-        return new ResponseEntity<>(newUser, HttpStatus.OK);
+        userRepository.save(newUser);
+
+        return "/newUser";
     }
 
 }
