82f4aade156042742977f8030763fcf2cfed888e
Lisa Sun <c.lisa.sun@gmail.com>
2021-05-10 18:53:34 -0500

    Tested the post endpoints

--- apis/src/test/java/xyz/bookself/controllers/user/UserControllerTest.java
+++ apis/src/test/java/xyz/bookself/controllers/user/UserControllerTest.java
@@ -1,21 +1,31 @@
 package xyz.bookself.controllers.user;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.web.bind.annotation.RequestParam;
 import xyz.bookself.users.domain.User;
 import xyz.bookself.users.repository.UserRepository;
 
+import java.nio.charset.Charset;
+import java.time.LocalDate;
 import java.util.Optional;
 import java.util.UUID;
 
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -24,7 +34,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class UserControllerTest {
 
     private final String apiPrefix = "/v1/users";
-
+    public static final MediaType APPLICATION_JSON_UTF8 = new MediaType(MediaType.APPLICATION_JSON.getType(), MediaType.APPLICATION_JSON.getSubtype(), Charset.forName("utf8"));
     @Autowired
     private MockMvc mockMvc;
 
@@ -35,7 +45,7 @@ public class UserControllerTest {
     void givenUserExists_whenGetRequestedWithIdOnPath_thenUserShouldBeReturned()
             throws Exception {
         final User userExists = new User();
-        
+
         userExists.setUsername("dummyUserName");
         userExists.setEmail(("dummy@dummy.com"));
         userExists.setPasswordHash(UUID.randomUUID().toString().replace("-", ""));
@@ -46,14 +56,32 @@ public class UserControllerTest {
                 .andExpect(status().isOk());
     }
 
+
     @Test
-    void givenUserCreated_UserShouldBeReturned()
-            throws Exception
-    {
+    void newUserInputIsGiven_soANewUserIsCreated()
+            throws Exception {
+
+        final User userExists = new User();
         final String newUserEmail = "newUser@newNew.com";
-        final String newUserName = "I am new";
-        mockMvc.perform(get(apiPrefix + "/newUser/?newUserEmail=" + newUserEmail + "&newUserName=" + newUserName))
+        final String newUserName = "newUser";
+        userExists.setId(1);
+        userExists.setUsername(newUserEmail);
+        userExists.setEmail(newUserName);
+        userExists.setPasswordHash("123");
+
+        when(userRepository.save(userExists)).thenReturn(null);
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(SerializationFeature.WRAP_ROOT_VALUE, false);
+        ObjectWriter ow = mapper.writer().withDefaultPrettyPrinter();
+        String requestJson=ow.writeValueAsString(userExists);
+
+        mockMvc.perform(post(apiPrefix + "/new-user").contentType(APPLICATION_JSON_UTF8)
+                .content(requestJson))
                 .andExpect(status().isOk());
+
     }
 }
+
+
 
