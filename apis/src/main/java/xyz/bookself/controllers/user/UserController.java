22088984d661951ce2236737dbf75aac26ed5c70
Lisa Sun <c.lisa.sun@gmail.com>
2021-05-10 18:53:01 -0500

    Modified a user endpoint into a post mapping

--- apis/src/main/java/xyz/bookself/controllers/user/UserController.java
+++ apis/src/main/java/xyz/bookself/controllers/user/UserController.java
@@ -3,21 +3,25 @@ package xyz.bookself.controllers.user;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
 
 import xyz.bookself.users.domain.User;
 import xyz.bookself.users.repository.UserRepository;
 
+import javax.persistence.Column;
 import java.time.LocalDate;
+import java.util.Map;
 import java.util.UUID;
 
 @RestController
 @RequestMapping("/v1/users")
 @Slf4j
 public class UserController {
-    private final UserRepository userRepository;
 
+    private final UserRepository userRepository;
 
     @Autowired
     public UserController(UserRepository repository) {
@@ -30,15 +34,10 @@ public class UserController {
         return new ResponseEntity<>(user, HttpStatus.OK);
     }
 
-    @RequestMapping(value = "/newUser", method = RequestMethod.GET)
-    public ResponseEntity<User>  createNewUser(@RequestParam String newUserEmail, @RequestParam String newUserName){
-        User newUser = new User();
-        newUser.setUsername(newUserName);
-        newUser.setEmail(newUserEmail);
-        newUser.setPasswordHash(UUID.randomUUID().toString().replace("-", ""));
+    @PostMapping(value = "/new-user", consumes = {MediaType.APPLICATION_JSON_VALUE})
+    @ResponseBody
+    public ResponseEntity<User>createNewUser(@RequestBody User newUser){
 
-        LocalDate date = LocalDate.now();
-        newUser.setCreated(date);
         userRepository.save(newUser);
         return new ResponseEntity<>(newUser, HttpStatus.OK);
     }
