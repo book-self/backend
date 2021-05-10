9f828e95aed2e66fa7587fd99cf07f1f84512949
Lisa Sun <c.lisa.sun@gmail.com>
2021-05-10 18:52:35 -0500

    Modified and added a post mapping

--- apis/src/main/java/xyz/bookself/controllers/user/BookListController.java
+++ apis/src/main/java/xyz/bookself/controllers/user/BookListController.java
@@ -3,25 +3,25 @@ package xyz.bookself.controllers.user;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import xyz.bookself.config.BookselfApiConfiguration;
 import xyz.bookself.users.domain.BookList;
 import xyz.bookself.users.domain.BookListEnum;
+import xyz.bookself.users.domain.User;
 import xyz.bookself.users.repository.BookListRepository;
 
-import java.util.Collection;
-import java.util.ArrayList;
-import java.util.UUID;
+import java.nio.charset.Charset;
+import java.util.*;
 
 @RestController
-@RequestMapping("/v1/bookLists")
+@RequestMapping("/v1/book-lists")
 @Slf4j
 public class BookListController
 {
     private final BookListRepository bookListRepository;
-
     @Autowired
     public BookListController(BookListRepository repository) {
         this.bookListRepository = repository;
@@ -33,30 +33,46 @@ public class BookListController
         return new ResponseEntity<>(booklist, HttpStatus.OK);
     }
 
-    @GetMapping("/newBookLists")
-    public ResponseEntity<Collection<BookList>>generateBookList(){
-        Collection<BookList> newBookLists = new ArrayList<BookList>();
+    @PostMapping("/new-book-lists")
+    @ResponseBody
+    public ResponseEntity<BookList>generateBookList(){
         BookList newDNF = new BookList();
         newDNF.setId(UUID.randomUUID().toString().replace("-", "").substring(0, 24));
         newDNF.setListType(BookListEnum.DNF);
         bookListRepository.save(newDNF);
-
-        BookList newCurrentlyReading = new BookList();
-        newCurrentlyReading.setId(UUID.randomUUID().toString().replace("-", "").substring(0, 24));
-        newCurrentlyReading.setListType(BookListEnum.READING);
-        bookListRepository.save(newCurrentlyReading);
-
-        BookList newRead = new BookList();
-        newRead.setId(UUID.randomUUID().toString().replace("-", "").substring(0, 24));
-        newRead.setListType(BookListEnum.READ);
-        bookListRepository.save(newRead);
-
-        newBookLists.add(newDNF);
-        newBookLists.add(newRead);
-        newBookLists.add(newCurrentlyReading);
-
-        return new ResponseEntity<>(newBookLists, HttpStatus.OK);
-
+        return new ResponseEntity<>(newDNF, HttpStatus.OK);
     }
 
+    @PostMapping(value = "/add-book-to-list", consumes = {MediaType.APPLICATION_JSON_VALUE})
+    @ResponseBody
+    public ResponseEntity<BookList>addBookToList(@RequestBody BookIdListIdDTO bookIdListIdDTO)
+    {
+        BookList foundBookList = bookListRepository.findById(bookIdListIdDTO.getListId()).orElseThrow();
+        Set<String> booksInList = foundBookList.getBooks();
+        booksInList.add(bookIdListIdDTO.getBookId());
+        foundBookList.setBooks(booksInList);
+        return new ResponseEntity<>(foundBookList, HttpStatus.OK);
+    }
+}
+
+class BookIdListIdDTO
+{
+    private String bookId;
+    private String listId;
+
+    public String getBookId() {
+        return bookId;
+    }
+
+    public String getListId() {
+        return listId;
+    }
+
+    public void setBookId(String bookId) {
+        this.bookId = bookId;
+    }
+
+    public void setListId(String listId) {
+        this.listId = listId;
+    }
 }
