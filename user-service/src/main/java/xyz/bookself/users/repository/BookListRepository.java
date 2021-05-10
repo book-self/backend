496cfa3d8b8d27f4af97ad71811564d045a05974
Lisa Sun <c.lisa.sun@gmail.com>
2021-05-09 19:36:24 -0500

    Added in query to get all book ids that are in a list

--- user-service/src/main/java/xyz/bookself/users/repository/BookListRepository.java
+++ user-service/src/main/java/xyz/bookself/users/repository/BookListRepository.java
@@ -3,9 +3,11 @@ package xyz.bookself.users.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import xyz.bookself.users.domain.BookList;
-
 import java.util.Collection;
 
 public interface BookListRepository extends JpaRepository<BookList, String> {
+
+    @Query(nativeQuery = true, value = "SELECT * FROM books WHERE id IN (SELECT book_id FROM books_in_lists WHERE bookListId=?1) LIMIT ?2")
+    Collection<String> findAllBookIdInList(String bookListId, int limit);
 }
 
