fd2ff9e587356f5dc6545ac5d809a866c5cf5909
Kyle Olson <kyle@k-olson.org>
2021-05-11 21:59:51 -0400

    basic auth

---
+++ apis/src/main/java/xyz/bookself/config/BookselfBasicAuthConfiguration.java
@@ -0,0 +1,36 @@
+package xyz.bookself.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class BookselfBasicAuthConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user1").password("password1234").roles("USER");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
+}
--- buildSrc/src/main/java/config/Dependencies.java
+++ buildSrc/src/main/java/config/Dependencies.java
@@ -6,6 +6,7 @@ public class Dependencies {
     public static final String springBootStarterWebFlux = "org.springframework.boot:spring-boot-starter-webflux";
     public static final String springBootStarterActuator = "org.springframework.boot:spring-boot-starter-actuator";
     public static final String springBootStarterDataJpa = "org.springframework.boot:spring-boot-starter-data-jpa";
+    public static final String springBootStarterSecurity = "org.springframework.boot:spring-boot-starter-security";
     public static final String springBootStarterJdbc = "org.springframework.boot:spring-boot-starter-jdbc";
     public static final String springBootConfigurationProcessor = "org.springframework.boot:spring-boot-configuration-processor";
     public static final String flywayCore = "org.flywaydb:flyway-core";
