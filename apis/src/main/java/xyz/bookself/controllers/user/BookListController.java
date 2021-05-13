3b8a99d81a6f5bae061a00122094afd43c57d18c
Lisa Sun <c.lisa.sun@gmail.com>
2021-05-12 19:29:12 -0500

    Updated apis

--- apis/src/main/java/xyz/bookself/controllers/user/BookListController.java
+++ apis/src/main/java/xyz/bookself/controllers/user/BookListController.java
@@ -7,6 +7,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import xyz.bookself.books.domain.Book;
+import xyz.bookself.config.BookselfApiConfiguration;
 import xyz.bookself.users.domain.BookList;
 import xyz.bookself.users.domain.BookListEnum;
 import xyz.bookself.users.repository.BookListRepository;
@@ -19,9 +20,11 @@ import java.util.UUID;
 @RequestMapping("/v1/book-lists")
 @Slf4j
 public class BookListController {
+    private final BookselfApiConfiguration apiConfiguration;
     private final BookListRepository bookListRepository;
     @Autowired
-    public BookListController(BookListRepository repository) {
+    public BookListController(BookselfApiConfiguration configuration, BookListRepository repository) {
+        this.apiConfiguration = configuration;
         this.bookListRepository = repository;
     }
 
@@ -31,6 +34,12 @@ public class BookListController {
         return new ResponseEntity<>(booklist, HttpStatus.OK);
     }
 
+    @GetMapping("/get-books-in-list")
+    public ResponseEntity<Collection<String>>getAllBooksInList(@RequestParam String bookListId)
+    {
+        final Collection<String>booksInList = bookListRepository.findAllBookIdInList(bookListId,apiConfiguration.getMaxReturnedBooks());
+        return new ResponseEntity<>(booksInList, HttpStatus.OK);
+    }
     @GetMapping("/get-user-book-lists")
     public ResponseEntity<Collection<BookList>>getUserBookList(@RequestParam Integer userId)
     {
@@ -49,6 +58,27 @@ public class BookListController {
             newDNF.setUserId(userIdDTO.getUserId());
             bookListRepository.save(newDNF);
             userBookLists.add(newDNF);
+
+            final BookList toRead = new BookList();
+            toRead.setId(UUID.randomUUID().toString().replace("-", "").substring(0, 24));
+            toRead.setListType(BookListEnum.TO_READ);
+            toRead.setUserId(userIdDTO.getUserId());
+            bookListRepository.save(toRead);
+            userBookLists.add(toRead);
+
+            final BookList read = new BookList();
+            read.setId(UUID.randomUUID().toString().replace("-", "").substring(0, 24));
+            read.setListType(BookListEnum.TO_READ);
+            read.setUserId(userIdDTO.getUserId());
+            bookListRepository.save(read);
+            userBookLists.add(read);
+
+            final BookList current = new BookList();
+            current.setId(UUID.randomUUID().toString().replace("-", "").substring(0, 24));
+            current.setListType(BookListEnum.TO_READ);
+            current.setUserId(userIdDTO.getUserId());
+            bookListRepository.save(current);
+            userBookLists.add(current);
             return new ResponseEntity<>(userBookLists, HttpStatus.OK);
         }
         return new ResponseEntity<>(userBookLists, HttpStatus.CREATED);
@@ -61,6 +91,8 @@ public class BookListController {
         final Set<String> booksInList = foundBookList.getBooks();
         booksInList.add(bookIdListIdDTO.getBookId());
         foundBookList.setBooks(booksInList);
+
+        bookListRepository.save(foundBookList);
         return new ResponseEntity<>(foundBookList, HttpStatus.OK);
     }
 }
