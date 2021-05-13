631a4de3ff8f5e9e38979ec63ebbdecc36b8e7ff
Kyle Olson <kyle@k-olson.org>
2021-05-12 22:20:16 -0400

    added back security

--- apis/src/main/java/xyz/bookself/controllers/user/UserController.java
+++ apis/src/main/java/xyz/bookself/controllers/user/UserController.java
@@ -5,16 +5,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
+import xyz.bookself.security.BookselfUserDetails;
 import xyz.bookself.users.domain.User;
 import xyz.bookself.users.repository.UserRepository;
 
 import java.time.LocalDate;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/v1/users")
@@ -30,8 +30,27 @@ public class UserController {
 
     @GetMapping("/{id}")
     public ResponseEntity<User> getUser(@PathVariable Integer id) {
-        final User user = userRepository.findById(id).orElseThrow();
-        return new ResponseEntity<>(user, HttpStatus.OK);
+        // Get the currently logged in user
+        final var userDetails = Optional.ofNullable(SecurityContextHolder.getContext())
+                .map(SecurityContext::getAuthentication)
+                .map(Authentication::getPrincipal)
+                .filter(principal -> principal instanceof BookselfUserDetails)
+                .map(BookselfUserDetails.class::cast);
+
+        // If nobody is logged in, UNAUTHORIZED
+        if (userDetails.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+
+        // If somebody is logged in but is trying to access somebody else's profile, FORBIDDEN
+        if (!userDetails.get().getId().equals(id)) {
+            return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+        }
+
+        // Otherwise look up the user
+        return userRepository.findById(id)
+                .map(u -> new ResponseEntity<>(u, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
     @PostMapping(value = "/new-user", consumes = {MediaType.APPLICATION_JSON_VALUE})
